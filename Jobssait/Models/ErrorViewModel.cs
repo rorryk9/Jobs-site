eb2a1b6c873a320c8a7ba8faf3d3af0e15050ab5
kaloyan123 <39674135+kaloyan123@users.noreply.github.com>
2022-03-06 13:30:38 +0200

    basa na saita

---
+++ Jobssait/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Jobssait.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
